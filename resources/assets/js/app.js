f2cbd6bd1de10babfff48e984f5160c33270e909
stcoder <eni.basic@gmail.com>
2016-02-18 16:12:13 +0500

    сделал фиксацию навигации в верху экрана при скроле; настроил овл-слайдер; добавил три слайда о боевых стилях.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,69 @@
-// fullpage config
-$('#fullpage').fullpage({
-  scrollingSpeed: 600,
-  autoScrolling: false
-});
+/** Start Carousel Feature Slide **/
+var owlCrouselFeatureSlide = function() {
+
+  var owl = $('.owl-carousel');
+
+  owl.on('initialized.owl.carousel change.owl.carousel',function(elem){
+    var current = elem.item.index;
+    $(elem.target).find('.owl-item').eq(current).find('.__to-animate').removeClass('fadeInUp animated');
+  });
+  owl.on('initialized.owl.carousel changed.owl.carousel',function(elem){
+    setTimeout(function(){
+      var current = elem.item.index;
+      $(elem.target).find(".owl-item").eq(current).find(".__to-animate").addClass('fadeInUp animated');
+    }, 700);
+  });
+  owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: true,
+    dots: true,
+    autoHeight: true,
+    smartSpeed: 600,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    autoplayHoverPause: true,
+    navText: [
+      "<i class='glyphicon glyphicon-chevron-left owl-direction'></i>",
+      "<i class='glyphicon glyphicon-chevron-right owl-direction'></i>"
+    ]
+  });
+
+};
+/** End Carousel Feature Slide **/
+
+/** Start navigation **/
+var navigationSection = function() {
+
+};
+/** End navigation **/
+
+/** Start scroll **/
+var windowScroll = function() {
+  var lastScrollTop = 0;
+
+  $(window).scroll(function(event) {
+    var scrlTop = $(this).scrollTop();
+    var $navContainer = $('.navbar-container');
+
+    if (scrlTop > 500 && scrlTop <= 2000) {
+      if (!$navContainer.hasClass('navbar-fixed-top')) {
+        $navContainer.addClass('navbar-fixed-top slideInDown animated');
+      }
+    } else if (scrlTop <= 500) {
+      if ($navContainer.hasClass('navbar-fixed-top')) {
+        $navContainer.removeClass('slideInDown animated').addClass('slideOutUp animated');
+        setTimeout(function() {
+          $navContainer.removeClass('navbar-fixed-top slideOutUp animated');
+        }, 200);
+      }
+    }
+  });
+};
+/** End scroll **/
+
+// === Running
+owlCrouselFeatureSlide();
+windowScroll();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,149 @@
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Универсальные бойцы</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/bower_components/owl.carousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="/bower_components/owl.carousel/dist/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+<body>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+{{-- Start nav --}}
+<div class="navbar-container">
+  <div class="container">
+    <nav class="navbar navbar-default">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Универсальные бойцы</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span>О клубе</span></a></li>
+          <li><a href="#"><span>Расписание и стоимость</span></a></li>
+          <li><a href="#"><span>Лента событий</span></a></li>
+          <li><a href="#"><span>Медиа</span></a></li>
+          <li><a href="#"><span>Контакты</span></a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</div>
+{{-- End nav --}}
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+{{-- Start slider --}}
+<div id="slider" data-section="slider">
+  <div class="owl-carousel owl-carousel-fullwidth">
+    {{-- Start slide --}}
+    <div class="item" style="background-image: url(//img11.nnm.ru/4/3/3/7/9/2ca11af4c6716b84fab2cca8ea4.jpg)">
+      <div class="overlay"></div>
+      <div class="container" style="position: relative;">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center">
+            <div class="slide-wrapper">
+              <div class="slide-content">
+                <h1 class="slide-lead __to-animate">Вин Чун</h1>
+                <h2 class="slide-sub-lead __to-animate anim-delay-1-3">максимум эффективности при минимуме движений</h2>
+                <p class="__to-animate anim-delay-1-6">
+                  <a href="http://freehtml5.co/" target="_blank" class="btn btn-primary">Читать подробней</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- End slide --}}
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+    {{-- Start slide --}}
+    <div class="item" style="background-image: url(//img01.quesabesde.com/media/img/noti/0058/victor_fraile.jpg)">
+      <div class="overlay"></div>
+      <div class="container" style="position: relative;">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center">
+            <div class="slide-wrapper">
+              <div class="slide-content">
+                <h1 class="slide-lead __to-animate">Ушу-саньда</h1>
+                <h2 class="slide-sub-lead __to-animate anim-delay-1-3">спортивно-соревновательные поединки, проводимые в полный контакт, с добавлением бросковой техники</h2>
+                <p class="__to-animate anim-delay-1-6">
+                  <a href="http://freehtml5.co/" target="_blank" class="btn btn-primary">Читать подробней</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- End slide --}}
 
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
+    {{-- Start slide --}}
+    <div class="item" style="background-image: url(//bjj59.ru/images/fon.jpg)">
+      <div class="overlay"></div>
+      <div class="container" style="position: relative;">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2 text-center">
+            <div class="slide-wrapper">
+              <div class="slide-content">
+                <h1 class="slide-lead __to-animate">Джиу джитсу</h1>
+                <h2 class="slide-sub-lead __to-animate anim-delay-1-3">японская вольная борьба и особая система приёмов самозащиты без оружия</h2>
+                <p class="__to-animate anim-delay-1-6">
+                  <a href="http://freehtml5.co/" target="_blank" class="btn btn-primary">Читать подробней</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {{-- End slide --}}
+  </div>
+</div>
+{{-- End slider --}}
 
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+{{-- Start about clube --}}
+<div id="about-clube" data-section="about-clube">
+
+</div>
+{{-- End about clube --}}
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis, commodi consequuntur dolores facere iusto praesentium similique. Expedita magnam nemo, quis recusandae reprehenderit repudiandae veniam vitae voluptatibus. Aliquid incidunt, ratione!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad blanditiis, cumque distinctio dolorum ducimus enim error fuga fugit illo laudantium nesciunt nisi provident reiciendis reprehenderit sapiente tempora veniam voluptate!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi cupiditate dolores explicabo iure laboriosam, nisi officiis, porro qui recusandae tempora vitae? Impedit in officia perferendis sequi vitae voluptate, voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores nesciunt praesentium reprehenderit rerum veniam veritatis voluptatem. Cum est, modi natus nobis optio perferendis reprehenderit tempore! Doloribus eius perspiciatis qui?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi cupiditate deleniti eum facilis harum, maiores nisi odio quas quasi quidem rem tempora, tempore voluptatem voluptates! Architecto autem et temporibus?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis, commodi consequuntur dolores facere iusto praesentium similique. Expedita magnam nemo, quis recusandae reprehenderit repudiandae veniam vitae voluptatibus. Aliquid incidunt, ratione!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad blanditiis, cumque distinctio dolorum ducimus enim error fuga fugit illo laudantium nesciunt nisi provident reiciendis reprehenderit sapiente tempora veniam voluptate!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi cupiditate dolores explicabo iure laboriosam, nisi officiis, porro qui recusandae tempora vitae? Impedit in officia perferendis sequi vitae voluptate, voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores nesciunt praesentium reprehenderit rerum veniam veritatis voluptatem. Cum est, modi natus nobis optio perferendis reprehenderit tempore! Doloribus eius perspiciatis qui?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi cupiditate deleniti eum facilis harum, maiores nisi odio quas quasi quidem rem tempora, tempore voluptatem voluptates! Architecto autem et temporibus?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis, commodi consequuntur dolores facere iusto praesentium similique. Expedita magnam nemo, quis recusandae reprehenderit repudiandae veniam vitae voluptatibus. Aliquid incidunt, ratione!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad blanditiis, cumque distinctio dolorum ducimus enim error fuga fugit illo laudantium nesciunt nisi provident reiciendis reprehenderit sapiente tempora veniam voluptate!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi cupiditate dolores explicabo iure laboriosam, nisi officiis, porro qui recusandae tempora vitae? Impedit in officia perferendis sequi vitae voluptate, voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores nesciunt praesentium reprehenderit rerum veniam veritatis voluptatem. Cum est, modi natus nobis optio perferendis reprehenderit tempore! Doloribus eius perspiciatis qui?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi cupiditate deleniti eum facilis harum, maiores nisi odio quas quasi quidem rem tempora, tempore voluptatem voluptates! Architecto autem et temporibus?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis, commodi consequuntur dolores facere iusto praesentium similique. Expedita magnam nemo, quis recusandae reprehenderit repudiandae veniam vitae voluptatibus. Aliquid incidunt, ratione!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad blanditiis, cumque distinctio dolorum ducimus enim error fuga fugit illo laudantium nesciunt nisi provident reiciendis reprehenderit sapiente tempora veniam voluptate!</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi cupiditate dolores explicabo iure laboriosam, nisi officiis, porro qui recusandae tempora vitae? Impedit in officia perferendis sequi vitae voluptate, voluptatum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores nesciunt praesentium reprehenderit rerum veniam veritatis voluptatem. Cum est, modi natus nobis optio perferendis reprehenderit tempore! Doloribus eius perspiciatis qui?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi cupiditate deleniti eum facilis harum, maiores nisi odio quas quasi quidem rem tempora, tempore voluptatem voluptates! Architecto autem et temporibus?</p>
+    </div>
+  </div>
+</div>
+
+<script src="/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="/bower_components/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="/js/app.js"></script>
+</body>
 </html>
