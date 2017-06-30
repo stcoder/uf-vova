d2e878c1ae61ae9658885602764b11143e0c342f
 Сергей Тихонов <eni.basic@gmail.com>
2017-06-30 10:21:51 +0500

    Добавил кнопка соц. сетей для новости

--- resources/views/news/detail.blade.php
+++ resources/views/news/detail.blade.php
@@ -69,6 +69,16 @@
               <div class="news">
                 <div class="news-text">{!! Shortcode::compile($news->content) !!}</div>
                 <span class="news-date news-date--with-detail">Опубликовано {{ $news->published_at->formatLocalized('%d %B %Y в %H:%M') }}</span>
+                <script type="text/javascript">(function() {
+                  if (window.pluso)if (typeof window.pluso.start == "function") return;
+                  if (window.ifpluso==undefined) { window.ifpluso = 1;
+                    var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                    s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+                    s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
+                    var h=d[g]('body')[0];
+                    h.appendChild(s);
+                  }})();</script>
+                <div class="pluso" data-background="transparent" data-options="small,square,line,horizontal,counter,theme=04" data-services="vkontakte,odnoklassniki,facebook,twitter,google,moimir,email"></div>
               </div>
             </div>
         </div>
